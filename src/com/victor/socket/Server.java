c830e45c9e4cc34dd8e0d291b08ee2ba1bc2d8a9
vikquicker <vik3_3stavrik3@mail.ru>
2019-06-10 19:47:28 +0300

    add sout message

--- src/com/victor/socket/Server.java
+++ src/com/victor/socket/Server.java
@@ -1,5 +1,7 @@
 package com.victor.socket;
 
+import com.victor.io.InputOutput;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -8,9 +10,12 @@ public class Server {
     public static void connection() {
         //Создается новое подключение с портом 6000
         try (ServerSocket serverSocket = new ServerSocket(6000)) {
+            System.out.println("Server started on port " + 6000);
             //ожидание ответа от клиента
+            System.out.print("Waiting for client");
             Socket client = serverSocket.accept();
             System.out.print("Connection complited");
+            InputOutput.readInfoFromFile("file");
         } catch (IOException e) {
             e.printStackTrace();
         }
