d9f5db4522ec8330ec6e90539d9ea94752413f39
vikquicker <vik3_3stavrik3@mail.ru>
2019-06-19 20:34:20 +0300

    add class Person rewrite method readFromFile

--- src/com/victor/io/InputOutput.java
+++ src/com/victor/io/InputOutput.java
@@ -1,15 +1,19 @@
 package com.victor.io;
 
+import com.victor.Person;
+
 import java.io.*;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 
 public class InputOutput {
     //метод который читает из файла и сохраняет данные в коллекцию
-    public static List<String> readInfoFromFile(String fileName) {
+    public static HashSet<Person> readInfoFromFile(String fileName) {
         //создается коллекция для хранения телефонов
         List<String> phoneList = new ArrayList<>();
+        HashSet<Person> hashSet = new HashSet<>();
         String str;
         //создается поток для чтения из файла (try-with-resourses)
         try (BufferedReader in = new BufferedReader(new FileReader(fileName))) {
@@ -17,7 +21,25 @@ public class InputOutput {
             while ((str = in.readLine()) != null) {
                 phoneList.add(str);
             }
+            String string = phoneList.toString();
+            String[] array = str.split(" ");
+            for (int i = 0; i < array.length; i++) {
+                Person person = new Person();
+                if (array[i] == "[a-zA-Z]") {
+                    person.setName(array[i]);
+                    person.getName();
+                    hashSet.add(person);
+                } else if (array[i] == "[0-9]" || array[i].length() <= 4) {
+                    person.setId(Integer.parseInt(array[i]));
+                    person.getId();
+                    hashSet.add(person);
+                } else if (array[i] == "[0-9]" || array[i].length() > 4) {
+                    person.setPhone(Long.parseLong(array[i]));
+                    person.getPhone();
+                    hashSet.add(person);
+                }
 
+            }
         } catch (FileNotFoundException e1) {
             System.out.println("file not found!");
             e1.printStackTrace();
@@ -26,7 +48,7 @@ public class InputOutput {
             System.out.println("io exception!");
             e.printStackTrace();
         }
-        return phoneList;
+        return hashSet;
     }
 
     public static void writeInFile(List<String> list) {
