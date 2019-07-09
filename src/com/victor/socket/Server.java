b9e7e94072c7e2f8aa5eba809a98c3abb53bb31b
vikquicker <vik3_3stavrik3@mail.ru>
2019-07-09 21:15:21 +0300

    add method mergeList

--- src/com/victor/socket/Server.java
+++ src/com/victor/socket/Server.java
@@ -1,6 +1,7 @@
 package com.victor.socket;
 
 import com.victor.Person;
+import com.victor.Status;
 import com.victor.io.InputOutput;
 import com.victor.wrapper.Wrapper;
 
@@ -11,12 +12,14 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
 public class Server implements Runnable {
     static ServerSocket serverSocket;
     static HashSet<Person> list;
+    static HashSet<Person> clientList;
 
     public Server() {
         try {
@@ -31,7 +34,7 @@ public class Server implements Runnable {
         connection();
     }
 
-    public static void connection() {
+    public void connection() {
         Wrapper wrapper = new Wrapper();
         //Создается новое подключение с портом 6000
         try {
@@ -51,22 +54,40 @@ public class Server implements Runnable {
             ///
             wrapper.setPersonContainer(personSet);
             objectOutputStream.writeObject(wrapper);
-
-
             try {
                 wrapper = (Wrapper) objectInputStream.readObject();
             } catch (ClassNotFoundException e) {
                 System.out.println("server could not read clients answer...");
                 e.printStackTrace();
             }
-            list = wrapper.getPersonContainer();
-            for (Person person : list) {
+            clientList = wrapper.getPersonContainer();
+            for (Person person : clientList) {
                 System.out.println(person.getId() + " " + person.getName() + " " + person.getPhone());
             }
+            mergeList(clientList);
             InputOutput.writeInFile(list);
+            ///
             objectOutputStream.close();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private void mergeList(HashSet<Person> clientSet) {
+        for (Person element : clientSet) {
+            if (element.getStatusOfPerson().equals(Status.ADDED)){
+                element.setStatusOfPerson(null);
+                list.add(element);
+            }else if(element.getStatusOfPerson().equals(Status.DELETED)){
+                Iterator<Person> iterator =  clientSet.iterator();
+                Person person;
+                while(iterator.hasNext()){
+                    person = iterator.next();
+                    if (person.getId() == element.getId()){
+                        list.remove(person);
+                    }
+                }
+            }
         }
     }
 
